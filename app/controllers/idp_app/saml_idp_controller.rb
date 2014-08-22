946c65db9db82a948e7f47ebeb04523b275f1d35
Russell Reas <russell.reas@gmail.com>
2014-08-22 10:29:16 -0700

    Multi-values for group hard-code

--- app/controllers/idp_app/saml_idp_controller.rb
+++ app/controllers/idp_app/saml_idp_controller.rb
@@ -16,17 +16,18 @@ module IdpApp
     def attributes_provider(email)
       %Q{
 <saml:AttributeStatement>
-  <saml:Attribute Name="email">
-    <saml:AttributeValue>#{email}</saml:AttributeValue>
+  <saml:Attribute Name="email" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:basic">
+    <saml:AttributeValue xsi:type="xs:string">#{email}</saml:AttributeValue>
   </saml:Attribute>
-  <saml:Attribute Name="firstName">
-    <saml:AttributeValue>Perry</saml:AttributeValue>
+  <saml:Attribute Name="firstName" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:basic">
+    <saml:AttributeValue xsi:type="xs:string">Perry</saml:AttributeValue>
   </saml:Attribute>
-  <saml:Attribute Name="lastName">
-    <saml:AttributeValue>Elselvier</saml:AttributeValue>
+  <saml:Attribute Name="lastName" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:basic">
+    <saml:AttributeValue xsi:type="xs:string">Elselvier</saml:AttributeValue>
   </saml:Attribute>
-  <saml:Attribute Name="group">
-    <saml:AttributeValue>my-group</saml:AttributeValue>
+  <saml:Attribute Name="group" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:basic">
+    <saml:AttributeValue xsi:type="xs:string">my-group</saml:AttributeValue>
+    <saml:AttributeValue xsi:type="xs:string">another-group</saml:AttributeValue>
   </saml:Attribute>
 </saml:AttributeStatement>
       }
